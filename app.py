7c97db1ba2ece35a52a75d9510e281678f6682ad
Kaleab Alemayehu (Neo) <cipherlofi04@gmail.com>
2025-05-17 09:22:58 -0400

    feat: handling request getting opitions form it

--- app.py
+++ app.py
@@ -1,12 +1,43 @@
-from flask import Flask,request
+from flask import Flask,request,jsonify
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 
-@app.route("/upload", methods=["POST"])
+UPLOAD_FOLDER = 'uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+@app.route('/upload', methods=['POST'])
 def upload():
-    return request.data 
-    # if request.method == 'POST':
-    #     if "file" in request.files:
-    #         return "No file"
-    #     f = request.files['image']
-    #     f.save("./test/image.png")
+    if 'image' not in request.files:
+        return jsonify({'error': 'No image part in the request'}), 400
+    file = request.files['image']
+    to_remove = request.form.get('toRemove', 'false').lower() == 'true'
+
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+
+    if file:
+        # TODO: change to upload to cloudinary
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(UPLOAD_FOLDER, filename)
+
+        file.save(filepath)
+
+        if to_remove:
+            processed_path = remove_background(filepath)
+        else:
+            processed_path = filepath
+
+        file_url = f"http://localhost:5000/{processed_path}"
+
+        return jsonify({'url': file_url}), 200
+
+def remove_background(filepath):
+
+    # TODO: change to rembg but
+    # For now, i’ll just rename it to simulate processing
+    new_path = filepath.replace('.', '_nobg.')
+    os.rename(filepath, new_path)
+    return new_path
+
