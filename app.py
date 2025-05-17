d99d9993340b708b120983066d497e45a0f19301
Kaleab Alemayehu (Neo) <cipherlofi04@gmail.com>
2025-05-17 10:25:40 -0400

    feat: adding cloudinary image upload

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask,request,jsonify
+from dotenv import load_dotenv
 from werkzeug.utils import secure_filename
 import os
+import cloudinary
+import cloudinary.uploader
+from cloudinary.utils import cloudinary_url
+
+# loading env variables
+load_dotenv()
+print(os.environ.get("CLOUD_NAME", "NOPE"))
+
+# Configuration       
+cloudinary.config( 
+    cloud_name = os.environ["CLOUD_NAME"], 
+    api_key = os.environ["API_KEY"], 
+    api_secret = os.environ["API_SECRET"],
+    secure=True
+)
 
 app = Flask(__name__)
 
@@ -13,31 +29,37 @@ def upload():
         return jsonify({'error': 'No image part in the request'}), 400
     file = request.files['image']
     to_remove = request.form.get('toRemove', 'false').lower() == 'true'
+    filename = request.form.get('filename',"").lower()
+
+    if filename == "":
+        return jsonify({'error': 'No filename stated'}), 400
 
     if file.filename == '':
         return jsonify({'error': 'No selected file'}), 400
 
     if file:
-        # TODO: change to upload to cloudinary
-        filename = secure_filename(file.filename)
-        filepath = os.path.join(UPLOAD_FOLDER, filename)
-
-        file.save(filepath)
-
         if to_remove:
-            processed_path = remove_background(filepath)
+            image = remove_background(filename)
         else:
-            processed_path = filepath
+            image = file
 
-        file_url = f"http://localhost:5000/{processed_path}"
+        uploadedStr = upload_image(image, filename)
 
-        return jsonify({'url': file_url}), 200
+        return jsonify({'url': uploadedStr}), 200
 
 def remove_background(filepath):
-
     # TODO: change to rembg but
     # For now, i’ll just rename it to simulate processing
     new_path = filepath.replace('.', '_nobg.')
     os.rename(filepath, new_path)
     return new_path
 
+
+def upload_image(image, filename ):
+    # Upload an image
+    upload_result = cloudinary.uploader.upload(image,public_id=filename)
+    # print(upload_result["secure_url"])
+    # Optimize delivery by resizing and applying auto-format and auto-quality
+    optimize_url, _ = cloudinary_url(filename, fetch_format="auto", quality="auto")
+    print("optimized", optimize_url)
+    return optimize_url
